62b37f084c9c75e922c7df5e6ccdd83f04e5832d
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2023-07-31 12:40:40 -0400

    page looking good, need to update services section

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import troybohrer from "../../public/troybohrer.png"
+import project from '../../public/IMG_0217.jpg'
 
 export default function Home() {
   return (
@@ -27,6 +31,31 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-4xl py-2 text-teal-600 font-medium">Troy Bohrer</h2>
+            <h3 className="text-2xl py-2">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">I am a developer and data analyst.</p>
+          </div>
+          <div className="text-4xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className="relative mx-auto rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={troybohrer} layout="fill" objectFit="cover"/>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-600">Web design, Databases, AWS, Power Bi, Excel</p>
+          </div>
+          <div>
+            <div>
+              <Image src={project} width={100} height={100}/>
+            </div>
+          </div>
         </section>
       </main>
     </div>

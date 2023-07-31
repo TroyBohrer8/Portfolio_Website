7882a47063aa05cd892eb210f3a9afd282ad38dd
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2023-07-31 11:33:51 -0400

    nav setup

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -7,11 +8,27 @@ export default function Home() {
         <title>Troy Bohrer Portfolio</title>
         <meta name="description" content=""></meta>
       </Head>
-      
-      <main>
-        <section className="bg-blue-700 min-h-screen"></section>
-      </main>
 
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">Troy Bohrer</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-5"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }

cde6d415f41b14a9bf23be5968b1e1f75906aff4
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2023-07-31 10:23:01 -0400

    setup complete, need to fix bug in page.js

---
+++ my-app/src/app/page.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Troy Bohrer Portfolio</title>
+        <meta name="description" content=""></meta>
+      </Head>
+      <div>Hello, World</div>
+    </div>
+  );
+}

cf0e7028214925fd655a2ab324f5fa4cfd748a81
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2023-07-31 15:07:38 -0400

    did some styling for large screens

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -8,8 +8,10 @@ import {
 import Image from "next/image";
 import troybohrer from "../../public/troybohrer.png";
 import project from "../../public/IMG_0217.jpg";
+// import { useState } from "react";
 
 export default function Home() {
+  // const [darkMode, setDarkMode] = useState(false)
   return (
     <div>
       <Head>
@@ -17,8 +19,9 @@ export default function Home() {
         <meta name="description" content=""></meta>
       </Head>
 
-      <main className="bg-white px-10">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
+          {/* Navbar */}
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl">Troy Bohrer</h1>
             <ul className="flex items-center">
@@ -35,16 +38,17 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          {/* About Me */}
           <div className="text-center p-10">
-            <h2 className="text-4xl py-2 text-teal-600 font-medium">
+            <h2 className="text-4xl py-2 text-teal-600 font-medium md:text-6xl">
               Troy Bohrer
             </h2>
-            <h3 className="text-2xl py-2">Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h3 className="text-2xl py-2 md:text-3xl">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl">
               I am a developer and data analyst.
             </p>
           </div>
-          <div className="text-4xl flex justify-center gap-16 py-3 text-gray-600">
+          <div className="text-4xl flex justify-center gap-16 py-3 text-gray-600 max-w-xl">
             <AiFillTwitterCircle />
             <AiFillLinkedin />
             <AiFillGithub />
@@ -53,7 +57,7 @@ export default function Home() {
             <Image src={troybohrer} layout="fill" objectFit="cover" />
           </div>
         </section>
-
+        {/* Services */}
         <section>
           <div>
             <h3 className="text-3xl py-1">Services I offer</h3>
@@ -61,7 +65,7 @@ export default function Home() {
               Web design, Databases, AWS, Power Bi, Excel
             </p>
           </div>
-          <div>
+          <div className="lg:flex gap-10">
             {/* Card 1 */}
             <div className="text-center shadow-lg rounded-xl my-10">
               <h3 className="text-lg">Web Design</h3>
@@ -88,6 +92,68 @@ export default function Home() {
               <p className="text-gray-800 py-1">Blah</p>
               <p className="text-gray-800 py-1">Blah</p>
               <p className="text-gray-800 py-1">Blah</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3>Projects</h3>
+            <p>These are some of my projects I've been working on.</p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={project}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
             </div>
           </div>
         </section>
